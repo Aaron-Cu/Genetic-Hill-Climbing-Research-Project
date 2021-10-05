7e1720812e10d7666fce2eea1fd939a22c51d3fc
HighEyeCue <aarontcu@gmail.com>
2021-10-05 16:11:13 -0400

    Commenting the code

--- graph_environment.py
+++ graph_environment.py
@@ -1,11 +1,11 @@
-#An application for creating Undirected Fully Connected Weighted Graphs of any size
+#An class for creating Undirected Connected Weighted Graphs of any size.
 #
-
-
 class Graphiest:
     verticies = None
     adj_matrix = None
 
+    #Class Constructor, Initializes a adjacency matrix of size n by n specified by input perameters.
+    #Input: int vert for the number of verticies in the desired graph.
     def __init__(self, vert):
         verticies = vert
         self.adj_matrix = [[1 for i in range(verticies)] for j in range(verticies)]
@@ -17,18 +17,22 @@ class Graphiest:
                     self.adj_matrix[i][j] = 0
         self.print()
     
+    #Method for balancing the adjacency matrix of the graph. The matrix should always be balance since this is
+    #an undirected graph.
     def balance(self):
         for i in range(len(self.adj_matrix)):
             for j in range(0,i):
                     self.adj_matrix[j][i] = self.adj_matrix[i][j]
 
+    #A built in function for printing the adjacency matrix to the console output.
     def print(self):
         print()
         for row in self.adj_matrix:
             print(row)
         print()
 
-
+#Code for testing the Class Functions
+#Output in the console will be used to verify functionality
 print("Creating a Fully Connected Graph of size 5")
 G = Graphiest(5)
 print("Setting bottom Left to 7")
