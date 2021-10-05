9e9fd0eed94464ab42e330b247a9bc26bd6513e1
HighEyeCue <aarontcu@gmail.com>
2021-10-05 17:57:05 -0400

    formatting

--- graph_environment.py
+++ graph_environment.py
@@ -13,11 +13,14 @@ class Graphiest:
     verticies = None
     adj_matrix = None
 
-    # Class Constructor, Initializes a adjacency matrix of size n by n specified by input perameters.
-    # Input: int vert for the number of verticies in the desired graph.
+    # Class Constructor, Initializes a adjacency matrix 
+    # of size n by n specified by input perameters.
+    # Input: int vert for the number of verticies in 
+    # the desired graph.
     def __init__(self, vert):
         verticies = vert
-        self.adj_matrix = [[1 for i in range(verticies)] for j in range(verticies)]
+        self.adj_matrix = [[1 for i in range(verticies)] 
+            for j in range(verticies)]
         # print(adj_matrix)
         self.print()
         for i in range(len(self.adj_matrix)):
@@ -26,7 +29,8 @@ class Graphiest:
                     self.adj_matrix[i][j] = 0
         self.print()
 
-    # Method for balancing the adjacency matrix of the graph. The matrix should always be balance since this is
+    # Method for balancing the adjacency matrix of the graph. 
+    # The matrix should always be balance since this is
     # an undirected graph.
     def balance(self):
         for i in range(len(self.adj_matrix)):
@@ -40,11 +44,12 @@ class Graphiest:
                 # add some random value
                 if self.adj_matrix[i][j] != 0:
                     self.adj_matrix[i][j] = round_down(
-                        upper_bound - (random.random() * upper_bound), 4
-                    )  # rand(not 0, to upperbound)
+                        upper_bound - (random.random() * upper_bound)
+                        , 4)  # rand(not 0, to upperbound)
                     self.adj_matrix[j][i] = self.adj_matrix[i][j]
 
-    # A built in function for printing the adjacency matrix to the console output.
+    # A built in function for printing the adjacency matrix
+    #  to the console output.
     def print(self):
         print()
         for row in self.adj_matrix:
