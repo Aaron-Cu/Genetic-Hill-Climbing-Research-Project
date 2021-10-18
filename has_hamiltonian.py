5ce86af153fb821ae4f908038ff0d12b2cbeec6e
vespenegas <91216338+vespenegas@users.noreply.github.com>
2021-10-17 22:22:06 -0400

    time complexity/comments final

--- has_hamiltonian.py
+++ has_hamiltonian.py
@@ -1,6 +1,8 @@
 from graph_environment import Graphiest
 
-
+# total time complexity O(n!)
+# main operation is comparison, checking every graph node as valid positions
+# several other operations are addition, and assignments
 class hamie:
 
     has_cycle = None
@@ -10,16 +12,21 @@ class hamie:
         self.has_cycle = None
 
     # checks if a vertex can be added to the path
+    # one, if function used to check edge weight from current position to next position
+    # another for loop used to compare if the next vertex is repeated in the "path"
     # time complexity = O(n) + O(n) = O(n)
-    # one if function where ch;ecking position of the amount of edges there are inputed. with another for loop
-    # that goes through the array/list of path, to compare if the next vertex is repeated
-    # main mathematical "work" is comparison
+    # main mathematical opeartion: comparison
     def is_safe(self, next_v, graph, path, position):
-        # if the weight of the current position in the path to the next vertex is 0 returns false as not possible move
+        # if the weight of the current position in the path to the next vertex is 0 returns
+        # false as invalid move
+        # time complexity = O(n), looping through path array
+        # main operation: comparison
         if graph.get_edge_weight(path[position - 1], next_v) == 0:
             return False
 
         # checks to see if the next move has already been in the path making it an invalid move
+        # time complexity = O(n), looping through path array
+        # main operation: comparison
         for i in range(len(path)):
             if path[i] == next_v:
                 return False
@@ -27,6 +34,7 @@ class hamie:
         # returns true when both above conditions are false, meaning the next move is valid to put into the path
         return True
 
+    # main function of the class that has data structures and calls find_circuit
     def has_hamiltonian(self, graph, Start_v):
         self.has_cycle = False
         # list to keep track of visited vertices
@@ -37,30 +45,30 @@ class hamie:
         path.append(Start_v)
         visited[0] = True
 
-        # call find hamiltonian circuit
+        # call find_circuit
         self.find_circuit(graph, 1, path, visited)
         print(len(self.list_cycles))
 
+        # Reduce total amount of "paths" in half due to redundancy. not complete
         # for index in range(0, int(len(self.list_cycles)/2)):
         # self.list_cycles.pop()
 
         return self.has_cycle
 
-    #
-    #
+    # recrusive function to find all hamiltonian cycles
+    # time complexity = O(n!)
+    # main operation: comparison
     def find_circuit(self, graph, position, path, visited):
-        # once the path is same length as the graph lenght, and if there is an edge from the last vertex to the
-        # initial vertex, add final vertex which is the start vertex
-        #               print("the lengeth of the path is " + str(len(path)) + " the vert count is " + str(graph.vertices_count) +" and the cirrent path is ")
-        #               print(*path, sep = ", ")
-        #
+        # once the path is same length as the graph length, and if there is an
+        # edge from the last vertex to the initial vertex
+        # add final vertex which is the start vertex
+        # time complexity = O(n), loop through the path array
+        # main operation: addition
         if len(path) == graph.vertices_count:
-            #           print("here")
-            # this part is O(n). the main task is addition. once the path is full, it will add up all weights of the path
             if graph.get_edge_weight(path[len(path) - 1], path[0]) != 0:
                 path.append(path[0])
+
                 # once the final start node is re-added at the end, print the entire path
-                # print(*path, sep = ", ")
                 total_weight = 0
                 if len(path) <= 1:
                     return total_weight
@@ -71,14 +79,17 @@ class hamie:
                 self.has_cycle = True
 
         # other vertices where the path is not the same length as the graph length
-        # O(n!) going through a for loop of the number of vertices count. where that for loop, loops though another
-        # for loop. essentially a nested for loop, inside of recursive function. as the graph gets larger, multiplied
-        # by n size
+        # time complexity = O(n!)
+        # nested loop inside of recusive function
+        # main operation is assignment for outer loop
+        # main operation for inner loop is comparison
+        # for loop going through graph vertices count, with inner nested loop: is_safe
+        # that is looping through path array
         for v in range(graph.vertices_count):
-            #                       print(str(position) +" to " +str(v))
             if self.is_safe(v, graph, path, position) and not visited[v]:
                 path.append(v)
                 visited[v] = True
+                # recursion + 1 position
                 self.find_circuit(graph, position + 1, path, visited)
                 visited[v] = False
                 path.pop(len(path) - 1)
